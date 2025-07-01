9c5ba78b5582d1d11a1dba524e4be7ac66b4c35e
Anaptsk1 <anukiptskialadze@gmail.com>
2025-07-01 22:58:27 +0200

    every single tab page added.

---
+++ RickAndMorty-SwiftUI/View/AddPhotosView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPhotosView.swift
+//  RickAndMorty-SwiftUI
+//
+//  Created by Ana Ptskialadze on 30.06.25.
+//
+
+import SwiftUI
+
+struct AddPhotosView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPhotosView()
+}
---
+++ RickAndMorty-SwiftUI/View/ReelsView.swift
@@ -0,0 +1,18 @@
+//
+//  ReelsView.swift
+//  RickAndMorty-SwiftUI
+//
+//  Created by Ana Ptskialadze on 01.07.25.
+//
+
+import SwiftUI
+
+struct ReelsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReelsView()
+}
